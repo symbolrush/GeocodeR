17e83fce38eeb4c2260eda97b66c8d46f0de3215
Adrian Schmid <trompetenschmid@gmail.com>
2015-06-30 18:21:48 +0200

    v0.1 initial version

--- R/geocode.R
+++ R/geocode.R
@@ -1,24 +1,10 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Cmd + Shift + B'
-#   Check Package:             'Cmd + Shift + E'
-#   Test Package:              'Cmd + Shift + T'
+# geocode
 
-geokodiere <- function(address){
-        library(XML)
+geocode <- function(address){
         url <- paste ('http://maps.googleapis.com/maps/api/geocode/xml?address=', address, '&sensor=true', sep = "", collapse = NULL)
-        doc = xmlTreeParse(url, useInternal=TRUE)
-        lat = as.numeric(xmlValue(getNodeSet(doc, '//location/lat')[[1]]))
-        lng = as.numeric(xmlValue(getNodeSet(doc, '//location/lng')[[1]]))
-        Sys.sleep(1) # Warten, sonst gibt die Google API nur M?ll zur?ck..
+        doc = XML::xmlTreeParse(url, useInternal=TRUE)
+        lat = as.numeric(XML::xmlValue(XML::getNodeSet(doc, '//location/lat')[[1]]))
+        lng = as.numeric(XML::xmlValue(XML::getNodeSet(doc, '//location/lng')[[1]]))
+        Sys.sleep(0.2) # Google API only accepts 5 queries per second for free accounts
         return(data.frame(lat = lat, lng = lng))
 }
