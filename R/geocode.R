359e57e007beb15261d87e2ed92aac1293a5320c
Adrian Schmid <trompetenschmid@gmail.com>
2015-07-09 13:13:04 +0200

    changed function geocode() - accepts now vectors of addresses as input

--- R/geocode.R
+++ R/geocode.R
@@ -1,10 +1,14 @@
 # geocode
 
-geocode <- function(address){
-        url <- paste ('http://maps.googleapis.com/maps/api/geocode/xml?address=', address, '&sensor=true', sep = "", collapse = NULL)
-        doc = XML::xmlTreeParse(url, useInternal=TRUE)
-        lat = as.numeric(XML::xmlValue(XML::getNodeSet(doc, '//location/lat')[[1]]))
-        lng = as.numeric(XML::xmlValue(XML::getNodeSet(doc, '//location/lng')[[1]]))
-        Sys.sleep(0.2) # Google API only accepts 5 queries per second for free accounts
-        return(data.frame(lat = lat, lng = lng))
+geocode <- function(addresses){
+  lat <- c()
+  lng <- c()
+  for(i in 1:length(addresses)) {
+    url <- paste ('http://maps.googleapis.com/maps/api/geocode/xml?address=', addresses[i], '&sensor=true', sep = "", collapse = NULL)
+    doc = XML::xmlTreeParse(url, useInternal=TRUE)
+    lat[i] = as.numeric(XML::xmlValue(XML::getNodeSet(doc, '//location/lat')[[1]]))
+    lng[i] = as.numeric(XML::xmlValue(XML::getNodeSet(doc, '//location/lng')[[1]]))
+    Sys.sleep(0.2) # Google API only accepts 5 queries per second for free accounts
+  } 
+  return(data.frame(lat = lat, lng = lng))
 }
