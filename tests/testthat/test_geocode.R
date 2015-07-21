36b4d193048e7ce58138cbc87e2272ae02c509d9
Adrian Schmid <trompetenschmid@gmail.com>
2015-07-21 14:28:11 +0200

    'geocode': added test for multiple address support

--- tests/testthat/test_geocode.R
+++ tests/testthat/test_geocode.R
@@ -1,4 +1,9 @@
-test_that("Test whether function 'geocode' works", {
-        expect_equal(geocode("Steinstrasse 73, 8003, Zürich")$lat, 47.3660124)
-        expect_equal(geocode("Steinstrasse 73, 8003, Zürich")$lng, 8.5215106)
+test_that("Test whether function 'geocode' works with single address", {
+        expect_equal(geocode("Rosenbergstrasse 59, 9001 St.Gallen")$lat, 47.4229917)
+        expect_equal(geocode("Rosenbergstrasse 59, 9001 St.Gallen")$lng, 9.3675559)
+})
+
+test_that("Test whether function 'geocode' works with multiple addresses", {
+        expect_equal(geocode(c("Rosenbergstrasse 59, 9001 St.Gallen", "Bahnhofplatz, 9000 St.Gallen"))$lat, c(47.4229917, 47.4226419))
+        expect_equal(geocode(c("Rosenbergstrasse 59, 9001 St.Gallen", "Bahnhofplatz, 9000 St.Gallen"))$lng, c(9.3675559, 9.3693916))
 })
